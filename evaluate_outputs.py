f6d0feb77e617d95a5a1df038a80ff4d9b03fbcd
Olly <ollystyles@gmail.com>
2019-09-25 05:43:47 +0100

    Added file to get metrics from existing predictions

---
+++ evaluate_outputs.py
@@ -0,0 +1,56 @@
+import argparse
+import pandas as pd
+import utils
+import numpy as np
+
+box_names = []
+
+for i in range(1, 61):
+    box_names.append('x1_' + str(i))
+    box_names.append('y1_' + str(i))
+    box_names.append('x2_' + str(i))
+    box_names.append('y2_' + str(i))
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-gt', help='Path to ground truth directory')
+parser.add_argument('-pred', help='Path to prediction directory')
+args = parser.parse_args()
+
+for detector in ['mask-rcnn', 'yolo']:
+    aious = []
+    fious = []
+    ades = []
+    fdes = []
+    for fold in [1, 2, 3]:
+
+        gt_df = pd.read_csv(args.gt + '/' + detector + '_fold_' +
+                            str(fold) + '.csv')
+        pred_df = pd.read_csv(args.pred + '/' + detector + '_fold_' +
+                              str(fold) + '.csv')
+
+        gt_boxes = gt_df[box_names].values.reshape(len(gt_df), -1, 4)
+        pred_boxes = pred_df[box_names].values.reshape(len(pred_df), -1, 4)
+
+        ade = utils.calc_ade(gt_boxes, pred_boxes)
+        fde = utils.calc_fde(gt_boxes, pred_boxes)
+        aiou = utils.calc_aiou(gt_boxes, pred_boxes)
+        fiou = utils.calc_fiou(gt_boxes, pred_boxes)
+
+        aious.append(aiou)
+        fious.append(fiou)
+        ades.append(ade)
+        fdes.append(fde)
+
+        print()
+        print(detector + ' fold ' + str(fold))
+        print('AIOU: ', round(aiou * 100, 1))
+        print('FIOU: ', round(fiou * 100, 1))
+        print('ADE:  ', round(ade, 1))
+        print('FDE:  ', round(fde, 1))
+
+    print()
+    print('Mean over 3 folds for ' + detector)
+    print('AIOU: ', round(np.mean(aious) * 100, 1))
+    print('FIOU: ', round(np.mean(fious) * 100, 1))
+    print('ADE:  ', round(np.mean(ades), 1))
+    print('FDE:  ', round(np.mean(fdes), 1))
---
+++ utils.py
@@ -0,0 +1,124 @@
+import numpy as np
+
+
+def calc_fde(outputs, targets):
+    '''
+    Calculates the final displacement between outputs and
+    targets centroids
+    Args:
+        outputs: np array. 3D array format (trajectory) x (timestep) x (x1y1x2y2)
+        targets: np array. 3D array format (trajectory) x (timestep) x (x1y1x2y2)
+    Returns:
+        float: Final displacement error between outputs and targets
+    '''
+    out_mid_xs = (outputs[:, - 1, 2] + outputs[:, - 1, 0]) / 2
+    out_mid_ys = (outputs[:, - 1, 3] + outputs[:, - 1, 1]) / 2
+
+    tar_mid_xs = (targets[:, - 1, 2] + targets[:, - 1, 0]) / 2
+    tar_mid_ys = (targets[:, - 1, 3] + targets[:, - 1, 1]) / 2
+
+    diff = ((out_mid_xs - tar_mid_xs) * (out_mid_xs - tar_mid_xs)) + \
+        ((out_mid_ys - tar_mid_ys) * (out_mid_ys - tar_mid_ys))
+
+    return float(np.mean(np.sqrt(diff)))
+
+
+def calc_ade(outputs, targets):
+    '''
+    Calculates the average displacement between outputs and
+    targets centroids
+    Args:
+        outputs: np array. 3D array format (trajectory) x (timestep) x (x1y1x2y2)
+        targets: np array. 3D array format (trajectory) x (timestep) x (x1y1x2y2)
+    Returns:
+        1D array: Average IOU between outputs and targets
+    '''
+    out_mid_xs = (outputs[:, :, 2] + outputs[:, :, 0]) / 2
+    out_mid_ys = (outputs[:, :, 3] + outputs[:, :, 1]) / 2
+
+    tar_mid_xs = (targets[:, :, 2] + targets[:, :, 0]) / 2
+    tar_mid_ys = (targets[:, :, 3] + targets[:, :, 1]) / 2
+
+    diff = ((out_mid_xs - tar_mid_xs) * (out_mid_xs - tar_mid_xs)) + \
+        ((out_mid_ys - tar_mid_ys) * (out_mid_ys - tar_mid_ys))
+
+    return np.mean(np.sqrt(np.mean(diff, axis=1)))
+
+
+def calc_fiou(outputs, targets):
+    '''
+    Calculates the final IOU between outputs and
+    targets
+    Args:
+        outputs: np array. 3D array format (trajectory) x (timestep) x (x1y1x2y2)
+        targets: np array. 3D array format (trajectory) x (timestep) x (x1y1x2y2)
+    Returns:
+        1D array: Final IOU between outputs and targets
+    '''
+    final_outputs = outputs[:, -1, :]
+    final_targets = targets[:, -1, :]
+    fiou = get_iou(final_outputs, final_targets)
+    return np.mean(fiou)
+
+
+def calc_aiou(outputs, targets):
+    '''
+    Calculates the average IOU between outputs and
+    targets
+    Args:
+        outputs: np array. 3D array format (trajectory) x (timestep) x (x1y1x2y2)
+        targets: np array. 3D array format (trajectory) x (timestep) x (x1y1x2y2)
+    Returns:
+        1D array: Average IOU between outputs and targets
+    '''
+    ious = np.zeros((targets.shape[0], targets.shape[1]))
+    for t in range(targets.shape[1]):
+        t_outputs = outputs[:, t, :]
+        t_targets = targets[:, t, :]
+        t_iou = get_iou(t_outputs, t_targets)
+        ious[:, t] = t_iou
+    return np.mean(np.mean(ious, axis=1))
+
+
+def get_iou(bboxes1, bboxes2):
+    """
+    Adapted from https://gist.github.com/zacharybell/8d9b1b25749fe6494511f843361bb167
+    Calculates the intersection-over-union of two bounding boxes.
+    Args:
+        bbox1 (numpy.array,  list of floats): bounding box in format x1,y1,x2,y2.
+        bbox2 (numpy.array, list of floats): bounding box in format x1,y1,x2,y2.
+    Returns:
+        int: intersection-over-onion of bbox1, bbox2
+    """
+    ious = []
+    for bbox1, bbox2 in zip(bboxes1, bboxes2):
+        bbox1 = [float(x) for x in bbox1]
+        bbox2 = [float(x) for x in bbox2]
+        (x0_1, y0_1, x1_1, y1_1) = bbox1
+        (x0_2, y0_2, x1_2, y1_2) = bbox2
+        # get the overlap rectangle
+        overlap_x0 = max(x0_1, x0_2)
+        overlap_y0 = max(y0_1, y0_2)
+        overlap_x1 = min(x1_1, x1_2)
+        overlap_y1 = min(y1_1, y1_2)
+        # check if there is an overlap
+        if overlap_x1 - overlap_x0 <= 0 or overlap_y1 - overlap_y0 <= 0:
+            ious.append(0)
+            continue
+        # if yes, calculate the ratio of the overlap to each ROI size and the unified size
+        size_1 = (x1_1 - x0_1) * (y1_1 - y0_1)
+        size_2 = (x1_2 - x0_2) * (y1_2 - y0_2)
+        size_intersection = (overlap_x1 - overlap_x0) * (overlap_y1 - overlap_y0)
+        size_union = size_1 + size_2 - size_intersection
+        iou = size_intersection / size_union
+        ious.append(iou)
+    return ious
+
+
+def cxcywh_to_x1y1x2y2(boxes):
+    old_boxes = boxes.copy()
+    boxes[:, 0, :] = old_boxes[:, 0, :] - (old_boxes[:, 2, :] / 2)
+    boxes[:, 2, :] = old_boxes[:, 0, :] + (old_boxes[:, 2, :] / 2)
+    boxes[:, 1, :] = old_boxes[:, 1, :] - (old_boxes[:, 3, :] / 2)
+    boxes[:, 3, :] = old_boxes[:, 1, :] + (old_boxes[:, 3, :] / 2)
+    return boxes
